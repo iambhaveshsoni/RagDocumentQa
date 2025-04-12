4f8732f28a8407915926c553e68dc027f10abdde
iambhaveshsoni <iambhavesh369@gmail.com>
2025-04-12 15:13:33 +0530

    new change

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import streamlit as st
 import os
 from langchain_groq import ChatGroq
 from langchain_openai import OpenAIEmbeddings
-from langchain_community.embeddings import OllamaEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
@@ -18,6 +17,8 @@ os.environ['OPENAI_API_KEY']=os.getenv("OPENAI_API_KEY")
 os.environ['GROQ_API_KEY']=os.getenv("GROQ_API_KEY")
 
 groq_api_key=os.getenv("GROQ_API_KEY")
+st.secrets["OPENAI_API_KEY"] == os.getenv("OPENAI_API_KEY")
+st.secrets["GROQ_API_KEY"] == os.getenv("GROQ_API_KEY")
 
 llm=ChatGroq(groq_api_key=groq_api_key,model_name="Llama3-8b-8192")
 
